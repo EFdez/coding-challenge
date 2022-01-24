9e29d70d3d55efb287322f66e4316df6e6dc6f36
EFdez <escarlafdez@gmail.com>
2022-01-24 21:30:46 +0100

    added fonts, navbar component, rules route and component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as MandoBeibiIcon } from "../../assets/images/Mando-Beibi.svg";
+import { Grid, Cell } from "react-foundation";
+
+import "./index.scss";
 
 const Navbar = () => {
   return (
-    <>
-      <Link to="/rules">Reglas del juego</Link>
-      <Link to="/pieces">Piezas</Link>
-      <Link to="/game">Juego</Link>
-    </>
+    <Grid className="Navbar display">
+      <Cell large={2} small={4} className="Navbar_icon">
+        <NavLink className={({ isActive }) => isActive ? "active" : undefined} to="/">
+          <MandoBeibiIcon />
+        </NavLink>
+      </Cell>
+      <Cell
+        large={9}
+        small={6}
+        className="Navbar_links text-mandalore text-big"
+      >
+        <NavLink className={({ isActive }) => isActive ? "active" : undefined} to="/rules">
+          <span>Reglas del juego</span>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => isActive ? "active" : undefined}
+          to="/pieces"
+        >
+          <span>Fichas</span>
+        </NavLink>
+        <NavLink className={({ isActive }) => isActive ? "active" : undefined} to="/game">
+          <span>Juego</span>
+        </NavLink>
+      </Cell>
+    </Grid>
   );
 };
 
